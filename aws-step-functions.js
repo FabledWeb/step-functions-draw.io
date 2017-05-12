e483101c2e3b6142ea5dd855f687b4a4794c5d8e
Eddie Canales <eddie.canales@crosschx.com>
2017-05-12 11:10:59 -0400

    bump aws sdk to 2.50.0

--- aws-step-functions.js
+++ aws-step-functions.js
@@ -1,6 +1,6 @@
 mxscript("https://cdn.rawgit.com/soney/jsep/b8baab7b/build/jsep.min.js");
 mxscript("https://cdn.rawgit.com/nodeca/js-yaml/9c1894e2/dist/js-yaml.min.js");
-mxscript("https://sdk.amazonaws.com/js/aws-sdk-2.9.0.min.js");
+mxscript("https://sdk.amazonaws.com/js/aws-sdk-2.50.0.min.js");
 // mxscript("https://rawgit.com/soney/jsep/master/build/jsep.min.js");
 //mxscript("https://localhost:8000/js/aws-sdk-2.9.0.js");
 //mxscript("https://localhost:8000/js/jsep.min.js");
